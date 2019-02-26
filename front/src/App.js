445fb76273c1fd50d4cf81f0d2513a6a03595bd0
Максим <max.v@mail.ru>
2019-02-26 23:53:28 +0300

    web: базовая логика перенесена из index.js в App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
-// import './App.css';
-import { Router, Route, hashHistory } from 'react-router';
-import HomePage from './pages/HomePage';
-import TestPage from './pages/TestPage';
-import BasePage from './pages/BasePage';
+// // import './App.css';
 
-// TODO: Подключить этот компонент и вынести в него что-нибудь, что было бы потом удобно тестировать.
+import BaseListPage from './pages/BaseListPage';
+import TestPage from './pages/TestPage.js';
+import { Provider } from 'react-redux';
+import store from './store';
+import BasePage from './pages/BasePage';
+import ImagePage from './pages/ImagePage';
+
+import {
+    HashRouter,
+    Route,
+    Link
+} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h3> hello </h3>
-          <Router history={hashHistory}>
-            <Route path="/" component={HomePage}/>
-            <Route path="/test" component={TestPage}/>
-            <Route path="/base/:id" component={BasePage} />
-          </Router>
-      </div>
-    );
+      <Provider store={store}>
+      <HashRouter>
+          <div>
+              <Link to={'/'}>Home</Link>
+              <Link to={'/test'}>Test</Link>
+              <Route exact path="/" component={BaseListPage} />
+              <Route path="/test" component={TestPage} />
+              <Route path="/base/:id" component={BasePage} />
+              <Route path="/image/:id" component={ImagePage} />
+          </div>
+      </HashRouter >
+  </Provider>
+  );
   }
 }
 
--- front/src/index.js
+++ front/src/index.js
@@ -1,34 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+// import './index.css';
 import * as serviceWorker from './serviceWorker';
-import BaseListPage from './pages/BaseListPage';
-import TestPage from './pages/TestPage.js';
-import { Provider } from 'react-redux';
-import store from './store';
-import BasePage from './pages/BasePage';
-import ImagePage from './pages/ImagePage';
 
-import {
-    HashRouter,
-    Route,
-    Link
-} from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render((
-    <Provider store={store}>
-        <HashRouter>
-            <div>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/test'}>Test</Link>
-                <Route exact path="/" component={BaseListPage} />
-                <Route path="/test" component={TestPage} />
-                <Route path="/base/:id" component={BasePage} />
-                <Route path="/image/:id" component={ImagePage} />
-            </div>
-        </HashRouter >
-    </Provider>
+    <App></App>
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
