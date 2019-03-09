08e942fb7a1ef8810ec1252cd579d9fce5fb120f
Максим <max.v@mail.ru>
2019-03-09 20:52:34 +0300

    web: minor css ListItem update

--- front/src/containers/ListItem.js
+++ front/src/containers/ListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ListItem.css';
 // import { Link } from 'react-router-dom';
 
 export default class ListItem extends React.Component {
     render() {
         return (
-            <div onClick={() => { this.props.onClick(this.props.base)}}>
+            <div className="List-item" onClick={() => { this.props.onClick(this.props.base)}}>
                 {/* <Link to={link}>{ this.props.base.title }</Link> */}
                 { this.props.base.title }
             </div>
