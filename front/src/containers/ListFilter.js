0a3a443805c352baea1a0481dad3424d79bd31f5
Максим <max.v@mail.ru>
2019-03-25 00:53:52 +0300

    debounce to search field added

--- front/src/containers/ListFilter.js
+++ front/src/containers/ListFilter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import debounce from '../misc/debounce';
 
 // module.exports = class extends React.Component {
 // export default class ListFilter extends React.Component {
@@ -21,25 +22,48 @@ export default class extends React.Component {
         this.props.updateRequest(newRequest);
     }
 
-    static getDerivedStateFromProps(props) {
+    // static getDerivedStateFromProps(props) {
+    getInitialState (props) {
+        // TODO: getInitialState не может обновить state, после инициализации компонента
+        // при изменении props. Нужно найти способ обновления state так, чтобы он не флудил как
+        // случае с getDerivedStateFromProps
+        // изучить https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
         // TODO: найти причину, по которой props.request.filter может передаваться сюда неопределенным.
         // TODO: исправить эту ситуацию.
+        console.log ('update state from props:', props);
         return { filter: props.request.filter || '' };
     }
 
-    onFilterChange = evt => {
-        let newRequest = { 
-            pageIndex: this.props.request.pageIndex,
-            pageSize: this.props.request.limit
-        };
-        if (this.props.request.filter) {
-            newRequest.filter = this.props.request.filter;
-        }
-        // TODO: здесь и в методе выше используется один и тот же код составления запроса.
-        // Его необходимо объединить.
-        newRequest['filter'] = evt.target.value;
-        this.props.updateRequest(newRequest);
+    onFilterChange2 = evt => {
+        // console.log ('onFilterChangeStart with value:', evt.target.value);
+        const newState = {...this.state, filter: evt.target.value};
+        // console.log ('setting new state:', newState);
+        this.setState(newState, () => { console.log ('state set: ', this.state)});
     };
+
+    onFilterChange = (evt) => {
+        // console.log ('onFilterChangeStart with value:', evt.target.value);
+        const newState = {...this.state, filter: evt.target.value};
+        // console.log ('setting new state:', newState);
+        this.setState(newState, () => { /* console.log ('state set: ', this.state)*/ });
+        // console.log ('start debounce');
+        this.filterDebounce(evt.target.value);
+    }
+
+    filterDebounce = debounce(value => {
+        // console.log ('debounce fired with value');
+            let newRequest = { 
+                pageIndex: this.props.request.pageIndex,
+                pageSize: this.props.request.limit
+            };
+            if (this.props.request.filter) {
+                newRequest.filter = this.props.request.filter;
+            }
+            // TODO: здесь и в методе выше используется один и тот же код составления запроса.
+            // Его необходимо объединить.
+            newRequest['filter'] = this.state.filter;
+            this.props.updateRequest(newRequest);
+    }, 500);
 
     render() {
         const params = this.props.request;
